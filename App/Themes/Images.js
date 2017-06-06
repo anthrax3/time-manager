c3e395cfffc5a7cc533a53c6abe833f61099edd1
Kevin Chappell <kevin.b.chappell@gmail.com>
2017-06-06 15:38:14 +0100

    remove unused assets

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -4,8 +4,6 @@
 const images = {
   logo: require('../Images/aurity-full-logo.png'),
   clearLogo: require('../Images/aurity-logo.png'),
-  ignite: require('../Images/ignite_logo.png'),
-  tileBg: require('../Images/tile_bg.png'),
   background: require('../Images/app-bg.png')
 }
 
